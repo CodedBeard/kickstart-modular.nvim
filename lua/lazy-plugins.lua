4371c9d4b0140ca639e0e5b3749f790521c75ce2
CodedBeard <codedbeard@users.noreply.github.com>
2024-03-12 13:45:12 +0000

    add dotnet stuff and keymaps

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -47,7 +47,8 @@ require('lazy').setup({
   require 'kickstart/plugins/mini',
 
   require 'kickstart/plugins/treesitter',
-
+  
+  require 'kickstart/plugins/harpoon',
   -- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
   -- put them in the right spots if you want.
@@ -57,8 +58,8 @@ require('lazy').setup({
   --  Here are some example plugins that I've included in the kickstart repository.
   --  Uncomment any of the lines below to enable them (you will need to restart nvim).
   --
-  -- require 'kickstart.plugins.debug',
-  -- require 'kickstart.plugins.indent_line',
+  require 'kickstart.plugins.debug',
+  require 'kickstart.plugins.indent_line',
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
